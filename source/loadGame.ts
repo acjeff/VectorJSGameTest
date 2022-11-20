1b1cbcfde013e6bd17d833dd01d863bb5ccc74a2
Alexander Jefferies <labaia33@gmail.com>
2022-11-20 21:43:25 +0000

    Drawing and Zooming

--- source/loadGame.ts
+++ source/loadGame.ts
@@ -14,132 +14,201 @@ interactive.originX = 0;
 interactive.originY = 0;
 interactive.border = true;
 
-let scale: number = 0.1,
-    startingPosX: number = 0,
-    startingPosY: number = 0,
-    cameraSpeed: number = Math.round((4 * scale) * 100) / 100,
-    moveSpeed: number = Math.round((5 * scale) * 100) / 100,
-    jumpingSpeed: number = Math.round((20 * scale) * 100) / 100,
-    gravity: number = Math.round((15 * scale) * 100) / 100,
-    playerHeight: number = Math.round((50 * scale) * 100) / 100,
-    playerWidth: number = Math.round((50 * scale) * 100) / 100,
-    viewX: number = Math.round(interactive.x),
-    viewY: number = Math.round(interactive.y),
-    viewSize: number = 100,
-    cameraPadding: number = 10,
-    movingCameraToPlayerCenter: boolean = false,
-    drawingPlatform: boolean = false,
-    editingPlatformIndex: number = -1,
-    player = interactive.rectangle(startingPosX, startingPosY, playerWidth, playerHeight);
+let scale: number,
+    startingPosX: number,
+    startingPosY: number,
+    cameraSpeed: number,
+    moveSpeed: number,
+    jumpingSpeed: number,
+    gravity: number,
+    playerHeight: number,
+    playerWidth: number,
+    viewX: number,
+    viewY: number,
+    viewWidth: number,
+    viewHeight: number,
+    cameraPadding: number,
+    movingCameraToPlayerCenter: boolean,
+    drawingPlatform: boolean,
+    editingPlatformIndex: number,
+    jumpTime: number,
+    player,
+    o_playerX = 0,
+    o_playerY = 0
 
-player.style.fill = randomColour();
+function resetValues(newValues?) {
+    if (!newValues) newValues = {};
+    scale = newValues['scale'] || scale || 1
+    startingPosX = newValues['startingPosX'] || startingPosX || 0
+    startingPosY = newValues['startingPosY'] || startingPosY || 0
+    cameraSpeed = newValues['cameraSpeed'] || Math.round((4 * scale) * 100) / 100
+    moveSpeed = newValues['moveSpeed'] || Math.round((5 * scale) * 100) / 100
+    jumpingSpeed = newValues['jumpingSpeed'] || Math.round((20 * scale) * 100) / 100
+    gravity = newValues['gravity'] || gravity || Math.round((15 * scale) * 100) / 100
+    playerHeight = newValues['playerHeight'] || Math.round((50 * scale) * 100) / 100
+    playerWidth = newValues['playerWidth'] || Math.round((50 * scale) * 100) / 100
+    viewX = newValues['viewX'] || viewX || Math.round(interactive.x)
+    viewY = newValues['viewY'] || viewY || Math.round(interactive.y)
+    viewWidth = newValues['viewWidth'] || viewWidth || interactive.width
+    viewHeight = newValues['viewHeight'] || viewHeight || interactive.height
+    cameraPadding = newValues['cameraPadding'] || cameraPadding || 10
+    movingCameraToPlayerCenter = newValues['movingCameraToPlayerCenter'] || movingCameraToPlayerCenter || false
+    drawingPlatform = newValues['drawingPlatform'] || drawingPlatform || false
+    editingPlatformIndex = newValues['editingPlatformIndex'] || editingPlatformIndex || -1
+    jumpTime = newValues['jumpTime'] || 100
+    if (!player) player = interactive.rectangle(startingPosX, startingPosY, playerWidth, playerHeight);
+    else {
+        player.width = playerWidth;
+        player.height = playerHeight;
+        player.x = 0;
+        player.y = 0;
+    }
+
+    if (_platforms_interactive.length) resetPlatforms();
+    else placeInitialPlatforms(_platforms);
+}
+
 let _platforms_interactive = [];
 let _platforms = [
     {
-        width: 1100,
+        width: 2100,
+        o_width: 2100,
         height: 50,
+        o_height: 50,
         x: 0,
+        o_x: 0,
         y: interactive.height,
+        o_y: interactive.height,
         beingResized: false,
         selected: false
     },
     {
+        o_width: 100,
         width: 100,
+        o_height: 150,
         height: 150,
         x: 300,
+        o_x: 300,
         y: interactive.height - 150,
+        o_y: interactive.height - 150,
         beingResized: false,
         selected: false
     },
     {
         width: 100,
+        o_width: 100,
         height: 250,
+        o_height: 250,
         x: 500,
+        o_x: 500,
         y: interactive.height - 250,
+        o_y: interactive.height - 250,
         beingResized: false,
         selected: false
     },
     {
         width: 100,
+        o_width: 100,
         height: 300,
+        o_height: 300,
         x: 600,
+        o_x: 600,
         y: interactive.height - 300,
+        o_y: interactive.height - 300,
         beingResized: false,
         selected: false
     },
     {
         width: 100,
+        o_width: 100,
         height: 300,
+        o_height: 300,
         x: 800,
+        o_x: 800,
         y: interactive.height - 300,
+        o_y: interactive.height - 300,
         beingResized: false,
         selected: false
     },
     {
         width: 100,
+        o_width: 100,
         height: 300,
+        o_height: 300,
         x: 1000,
+        o_x: 1000,
         y: interactive.height - 300,
+        o_y: interactive.height - 300,
         beingResized: false
     }
 ]
 
-_platforms = _platforms.map((p) => {
-    p.width *= scale;
-    p.height *= scale;
-    p.x *= scale;
-    p.y *= scale;
-    return p;
-})
+resetValues();
+player.style.fill = randomColour();
+
+function setPlatforms() {
+    _platforms = _platforms.map((p) => {
+        p.width = p.o_width * scale;
+        p.height = p.o_height * scale;
+        p.x = p.o_x * scale;
+        p.y = p.o_y * scale;
+        return p;
+    })
+}
+
+function resetPlatforms() {
+    setPlatforms();
+    _platforms_interactive.forEach((p, i) => {
+        p.width = _platforms[i].width;
+        p.height = _platforms[i].height;
+        p.x = _platforms[i].x;
+        p.y = _platforms[i].y;
+    })
+}
 
 window.addEventListener("wheel", event => {
     let scrollAmount = event.deltaY;
-    if (scrollAmount > 0) scrollAmount = 10;
-    else (scrollAmount) = -10;
-    let newView = viewSize + scrollAmount;
-    if (newView > 500) newView = 500;
-    else if (newView < 30) newView = 30;
-    viewSize = Math.round(newView);
-    console.log(viewSize, ' : new view');
+    if (scrollAmount > 0) scrollAmount = 0.1;
+    else (scrollAmount) = -0.1;
+    resetValues({scale: Math.round((scale + scrollAmount) * 100) / 100});
     moveCameraToPlayerCenter();
 });
 
 interactive.root.onmousedown = event => {
-    console.log('Mouse Down');
-    console.log(event, ' : event');
-    console.log(event.x, ' : event x');
-    console.log(event.y, ' : event y');
-    console.log(viewX, ' : view x');
-    let group = document.getElementById('group-1');
-    let groupCoords = group.getBoundingClientRect();
-    console.log(group, ' : group');
-    console.log(group.getBoundingClientRect(), ' : bounding');
-    let x = (event.x - groupCoords.x) * scale;
-    console.log(x, ' : x')
-    if (!drawingPlatform) {
-        createPlatform({
-            width: 100 * scale,
-            height: 100 * scale,
-            x: x,
-            y: (interactive.height - 100) * scale
-        })
-        editingPlatformIndex = _platforms_interactive.length - 1;
-        drawingPlatform = true;
-    }
+    drawingPlatform = true;
 };
 
 interactive.root.onmouseup = event => {
-    console.log('Mouse Up');
     drawingPlatform = false;
     editingPlatformIndex = -1;
 };
 
 interactive.root.onmousemove = event => {
-    if (drawingPlatform && editingPlatformIndex > -1) {
-        let platformBeingEdited = _platforms_interactive[editingPlatformIndex];
-        // platformBeingEdited.width = event.x - platformBeingEdited.x;
-        // platformBeingEdited.height = event.y - platformBeingEdited.y;
+    if (drawingPlatform) {
+        createPlatform({
+            width: 10,
+            o_width: 10,
+            height: 10,
+            o_height: 10,
+            x: ((event.x + viewX) - 5) / scale,
+            o_x: ((event.x + viewX) - 5) / scale,
+            y: ((event.y + viewY) - 5) / scale,
+            o_y: ((event.y + viewY) - 5) / scale
+        }, '#000000', true);
+        editingPlatformIndex = _platforms_interactive.length - 1;
     }
+
+};
+
+window.onkeydown = function (event) {
+    if (event.keyCode === 32 && !stateEngine.states.jumping && stateEngine.states.touchingSurface.bottom) triggerJump();
+    if (event.keyCode === 65 || event.keyCode === 37) stateEngine.states.movingLeft = true;
+    if (event.keyCode === 68 || event.keyCode === 39) stateEngine.states.movingRight = true;
+};
+
+window.onkeyup = function (event) {
+    if (event.keyCode === 65 || event.keyCode === 37) stateEngine.states.movingLeft = false;
+    if (event.keyCode === 68 || event.keyCode === 39) stateEngine.states.movingRight = false;
 };
 
 let stateEngine = {
@@ -166,21 +235,27 @@ function randomColour() { // min and max included
     return 'rgb(' + randomColourValue() + ', ' + randomColourValue() + ', ' + randomColourValue() + ')';
 }
 
-function createPlatform(platform) {
+function createPlatform(platform, col?, freshy?) {
+    if (freshy) {
+        _platforms.push(platform);
+        setPlatforms();
+        platform = _platforms[_platforms.length - 1];
+    }
     _platforms_interactive.push(interactive.rectangle(platform.x, platform.y, platform.width, platform.height));
-    _platforms_interactive[_platforms_interactive.length - 1].style.fill = randomColour();
+    _platforms_interactive[_platforms_interactive.length - 1].style.fill = col || randomColour();
 }
 
 function placeInitialPlatforms(platforms) {
+    setPlatforms();
     platforms.forEach((platform) => {
         createPlatform(platform)
     });
 }
 
-function checkCollisions(onGround) {
+function checkCollisions() {
     let onLeft = _platforms_interactive.find((p) => player.x + -scale <= (p.x + p.width) && player.x + -scale > p.x && (player.y + (playerHeight / 2)) >= p.y);
     let onRight = _platforms_interactive.find((p) => (player.x + playerWidth) >= p.x + -scale && (player.y + (playerHeight / 2)) >= p.y && ((player.x + playerWidth) <= (p.x + -scale + (p.width))));
-    let onBottom = _platforms_interactive.find((p) => ((player.y + playerHeight) >= p.y + -scale) && ((player.x + playerWidth) >= (p.x)) && (player.x <= (p.x + p.width))) || onGround;
+    let onBottom = _platforms_interactive.find((p) => ((player.y + playerHeight) >= p.y + -scale) && ((player.x + playerWidth) >= (p.x)) && (player.x <= (p.x + p.width)));
     if (onRight && (player.x + playerWidth) >= onRight.x && (player.x + playerWidth) <= (onRight.x + onRight.width)) player.x = (onRight.x - playerWidth) - scale;
     if (onLeft && player.x <= (onLeft.x + onLeft.width) && player.x >= onLeft.x) player.x = (onLeft.x + onLeft.width) + scale;
     if (onBottom && (player.y + playerHeight) >= onBottom.y && (player.y + playerHeight) <= (onBottom.y + onBottom.height)) player.y = (onBottom.y - playerHeight) - scale;
@@ -209,56 +284,37 @@ function moveCamera() {
     if (middlePlayerY < middleCameraY - (!movingCameraToPlayerCenter ? cameraPadding : 0)) viewY -= (!movingCameraToPlayerCenter ? cameraSpeed : Math.round(cameraSpeed * 3));
 
     if (Math.round(middleCameraX) === Math.round(middlePlayerX) && Math.round(middleCameraY) === Math.round(middlePlayerY)) movingCameraToPlayerCenter = false;
-    interactive.setViewBox(viewX, viewY, viewSize, viewSize);
+    interactive.setViewBox(viewX, viewY, viewWidth, viewHeight);
 }
 
 function triggerJump() {
     stateEngine.states.jumping = true;
     window.setTimeout(() => {
         stateEngine.states.jumping = false;
-    }, 100);
+    }, jumpTime);
 }
 
-function movePlayer(onGround) {
+function movePlayer() {
     let jumpingForce = stateEngine.states.jumping ? 10 * scale : 0;
-
-    if (onGround) player.y = interactive.height - playerHeight;
 
     if (stateEngine.states.jumping) player.y -= jumpingSpeed;
     //Fall from Gravity
-    if (!onGround && !stateEngine.states.jumping && !stateEngine.states.touchingSurface.bottom) player.y += gravity;
+    if (!stateEngine.states.jumping && !stateEngine.states.touchingSurface.bottom) player.y += gravity;
     //Move Left
-    if (!(player.x <= 0) && !stateEngine.states.touchingSurface.left) if (stateEngine.states.movingLeft) player.x -= (moveSpeed + jumpingForce);
+    if (!stateEngine.states.touchingSurface.left) if (stateEngine.states.movingLeft) player.x -= (moveSpeed + jumpingForce);
     //Move Right
-    if (!(player.x >= (interactive.width - playerWidth)) && !stateEngine.states.touchingSurface.right) if (stateEngine.states.movingRight) player.x += (moveSpeed + jumpingForce);
+    if (!stateEngine.states.touchingSurface.right) if (stateEngine.states.movingRight) player.x += (moveSpeed + jumpingForce);
 
 }
 
 function runFrame() {
-    let onGround = (player.y >= (interactive.height - playerHeight));
+    checkCollisions();
 
-    checkCollisions(onGround);
+    movePlayer();
 
-    movePlayer(onGround);
-
-    moveCamera()
+    moveCamera();
 
     window.requestAnimationFrame(runFrame);
 }
 
-placeInitialPlatforms(_platforms);
-
 window.requestAnimationFrame(runFrame);
-
-window.onkeydown = function (event) {
-    if (event.keyCode === 32 && !stateEngine.states.jumping && stateEngine.states.touchingSurface.bottom) triggerJump();
-    if (event.keyCode === 65 || event.keyCode === 37) stateEngine.states.movingLeft = true;
-    if (event.keyCode === 83 || event.keyCode === 40) stateEngine.states.movingDown = true;
-    if (event.keyCode === 68 || event.keyCode === 39) stateEngine.states.movingRight = true;
-};
-
-window.onkeyup = function (event) {
-    if (event.keyCode === 65 || event.keyCode === 37) stateEngine.states.movingLeft = false;
-    if (event.keyCode === 83 || event.keyCode === 40) stateEngine.states.movingDown = false;
-    if (event.keyCode === 68 || event.keyCode === 39) stateEngine.states.movingRight = false;
-};
