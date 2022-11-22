f715f7e9999d2ba22309dc19444baa701cdcb69b
Alexander Jefferies <labaia33@gmail.com>
2022-11-22 21:05:53 +0000

    Start screen

--- source/StartScreen.ts
+++ source/StartScreen.ts
@@ -1,16 +1,11 @@
 import {startGame} from './loadGame.js'
 import {Interactive} from './index.js';
 
-console.log('StartScreen');
 let interactive = new Interactive('StartScreen')
 interactive.width = window.innerWidth - 30;
 interactive.height = window.innerHeight - 30;
 interactive.originX = 0;
 interactive.originY = 0;
 interactive.border = true;
-let newText = interactive.text(50, 50, 'Start Game');
 
-newText.root.onclick = event => {
-    interactive.remove();
-    startGame();
-};
+//PUT YOUR START SCREEN HERE :)
