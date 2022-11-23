d4c7c2d7c0fd1bcf4b5b0819ed64f9f18603e1e1
Jess E <jessenemark@gmail.com>
2022-11-23 20:54:59 +0000

    updated Start screen

--- source/StartScreen.ts
+++ source/StartScreen.ts
@@ -1,5 +1,6 @@
 import {startGame} from './loadGame.js'
 import {Interactive} from './index.js';
+import HoverBox from './elements/input/hover-box.js';
 
 let interactive = new Interactive('StartScreen')
 interactive.width = window.innerWidth - 30;
@@ -9,3 +10,15 @@ interactive.originY = 0;
 interactive.border = true;
 
 //PUT YOUR START SCREEN HERE :)
+
+let text = interactive.text(interactive.width / 3, interactive.height / 2, "SQUART");
+text.style.fontSize = '138px';
+text.style.textAlign = 'center';
+text.style.fontFamily = "monospace";
+
+
+let button = interactive.button(660, 550, "   ");
+
+let startText = interactive.text(725, 560, "Start Game");
+startText.style.fontSize = '30px'
+text.style.fontFamily = "monospace";
