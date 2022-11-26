4434c3a2055cf0b70a0c8674fd37455d51fd02d7
Jess E <jessenemark@gmail.com>
2022-11-26 20:17:44 +0000

    added animation and centered text

--- source/StartScreen.ts
+++ source/StartScreen.ts
@@ -3,26 +3,45 @@ import {Interactive} from './index.js';
 import HoverBox from './elements/input/hover-box.js';
 
 let interactive = new Interactive('StartScreen')
-interactive.width = window.innerWidth - 30;
-interactive.height = window.innerHeight - 30;
+interactive.width = window.innerWidth;
+interactive.height = window.innerHeight;
 interactive.originX = 0;
 interactive.originY = 0;
 interactive.border = true;
 
 //PUT YOUR START SCREEN HERE :)
 
-let text = interactive.text(0, 0, "SQUART");
-text.style.fontSize = '138px';
-text.style.textAlign = 'center';
-text.style.fontFamily = "monospace";
+let title = interactive.text(0, interactive.height / 2, "SQUART");
+title.style.fontSize = '138px';
+title.style.textAlign = 'center';
+title.style.fontFamily = "monospace";
+let textBbox = title.getBoundingBox();
+title.x = (interactive.width / 2) - (textBbox.width / 2);
 
-// let button = interactive.button(660, 550, "   ");
-// button.width = '100%';
-// button.height = '100%';
+let squareSize = 50;
+let squareX = (interactive.width / 2) - (squareSize / 2);
+let squareY = (interactive.height / 2) + (textBbox.height / 2);
+let animation;
+
+let square = interactive.rectangle(squareX, squareY, squareSize, squareSize);
+square.root.onclick = (event) => {
+    console.log(event);
+    square.fill = 'red';
+    let animation = window.requestAnimationFrame(moveSquare);
+};
+
+function moveSquare () {
+    if (square.y > interactive.height) {
+        window.cancelAnimationFrame(animation);
+        console.log('Done.');
+        return;
+    }
+    square.y += 10;
+    console.log(square.y);
+    window.requestAnimationFrame(moveSquare);
+}
 
 // let startText = interactive.text(725, 560, "Start Game");
-// startText.width = '100%';
-// startText.height = '100%';
 // startText.style.fontSize = '30px'
 // startText.textAlign = 'center';
 // text.style.fontFamily = "monospace";
